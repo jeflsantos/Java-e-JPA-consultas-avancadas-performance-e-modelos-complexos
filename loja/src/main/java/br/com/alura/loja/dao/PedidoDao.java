451b0f3c9018911230134927219518b2518e2ad9
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-16 00:07:01 -0300

    Java e JPA: consultas avançadas, performance e modelos complexos Java e JPA: consultas avançadas, performance e modelos complexos
    Consultas com select new

--- loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 public class PedidoDao {
 	
@@ -23,16 +24,17 @@ public class PedidoDao {
 		String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
 		return em.createQuery(jpql, BigDecimal.class).getSingleResult();
 	}
-	public List<Object[]> relatorioDeVendas(){
-		String jpql = "SELECT produto.nome, " 
+	public List<RelatorioDeVendasVo> relatorioDeVendas(){
+		String jpql = "SELECT new br.com.alura.loja.vo.RelatorioDeVendasVo(" 
+				+"produto.nome, "
 				+"SUM(item.quantidade), " 
-				+"MAX(pedido.data) "
+				+"MAX(pedido.data)) "
 				+"FROM Pedido pedido "
 				+"JOIN pedido.itens item "
 				+"JOIN item.produto produto "
 				+"GROUP BY produto.nome "
 				+"ORDER BY item.quantidade DESC ";
-		return em.createQuery(jpql, Object[].class).getResultList();
+		return em.createQuery(jpql, RelatorioDeVendasVo.class).getResultList();
 		
 		
 		
--- loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -15,6 +15,7 @@ import br.com.alura.loja.modelo.ItemPedido;
 import br.com.alura.loja.modelo.Pedido;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 public class CadastroDePedido {
 
@@ -45,12 +46,8 @@ public class CadastroDePedido {
 		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
 		System.out.println("Valor Total: " +totalVendido);
 		
-		List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
-		for (Object[] obj : relatorio) {
-			System.out.println(obj[0]);
-			System.out.println(obj[1]);
-			System.out.println(obj[2]);
-		}
+		List<RelatorioDeVendasVo> relatorio = pedidoDao.relatorioDeVendas();
+		relatorio.forEach(System.out::println);
 		
 		
 	}
